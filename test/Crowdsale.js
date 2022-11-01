85530e2a31e76ce1a69c65a0e69b259e17bd745d
Santiago restrepo <santiagorestrepo@santiagos-mbp-3.attlocal.net>
2022-11-01 01:04:26 -0400

    Adds Crowdsale Contract

---
+++ test/Crowdsale.js
@@ -0,0 +1,22 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens = (n) => {
+	return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe('Crowdsale', () => {
+	let crowdsale
+
+	beforeEach(async () => {
+		const Crowdsale = await ethers.getContractFactory('Crowdsale')
+		crowdsale = await Crowdsale.deploy()
+	})
+
+	describe('Deployment', () => {
+		it('has a name', async () => {
+			
+			expect(await crowdsale.name()).to.eq("Crowdsale")
+		})
+	})
+})
