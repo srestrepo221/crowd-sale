35a0d7c87d56d38f59b5adb6b60ebebe77039737
Santiago restrepo <santiagorestrepo@santiagos-mbp-3.attlocal.net>
2022-11-01 19:02:48 -0400

    Buy Tokens

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,7 +1,32 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract Crowdsale {
-	string public name = "Crowdsale";
+// Need code that is taken care of with import "./Token.sol";
+import "./Token.sol";
 
+contract Crowdsale {
+	//string public name = "Crowdsale";
+	Token public token;
+	uint256 public price;
+	uint256 public maxTokens;
+	uint256 public tokensSold; 
+
+	event Buy(uint256 amount, address buyer);
+	
+	// Need Address
+	constructor(Token _token, uint256 _price, uint256 _maxTokens){
+		token = _token;
+		price = _price;
+		maxTokens = _maxTokens;
+	}
+
+	function buyTokens(uint256 _amount) public payable {
+		require(msg.value == (_amount / 1e18) * price);
+		require(token.balanceOf(address(this)) >= _amount); 
+		require(token.transfer(msg.sender, _amount));
+
+		tokensSold += _amount;
+
+		emit Buy(_amount,msg.sender);
+	}
 }
