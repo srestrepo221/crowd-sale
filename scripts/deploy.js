a3b261c8f607b5b3602802c07215fb7d9db32688
Santiago restrepo <santiagorestrepo@santiagos-mbp-3.attlocal.net>
2022-11-02 01:57:01 -0400

    Adds smart contracts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,20 +7,30 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const NAME = 'Green Bros'
+  const SYMBOL = 'GB'
+  const MAX_SUPPLY = '21000000000'
+  const PRICE = ethers.utils.parseUnits('0.025', 'ether')
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  // Deploy Token
+  const Token = await hre.ethers.getContractFactory('Token')
+  let token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY)
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await token.deployed()
+  console.log(`Token deployed to: ${token.address}\n`)
 
-  await lock.deployed();
+  // Deploy Crowdsale
+  const Crowdsale = await hre.ethers.getContractFactory('Crowdsale')
+  const crowdsale = await Crowdsale.deploy(token.address, PRICE, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'))
+  await crowdsale.deployed();
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(`Crowdsale deployed to: ${crowdsale.address}\n`)
+
+  // Send tokens to crowdsale
+  const transaction = await token.transfer(crowdsale.address, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'))
+
+  console.log(`Tokens transferred to Crowdsale\n`)
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
